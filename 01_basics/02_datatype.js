3e52d62876a727f45e88b33b8968bdecc8919b44
mishra4567 <mishrapritam831@gmail.com>
2024-06-24 22:53:02 +0530

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,33 @@
+"use strict"; //treat all js code as newer version
+
+// alert("hello")
+//we are using nodejs, not browser
+
+/*
+console.log(3+3);
+// code readability should be high
+console.log("Pritam");
+*/
+
+let name="Pritam"
+let age = 18
+let isLoggedIn=false
+let state;
+let sity=null;
+
+
+// number => 2to power 53
+// bigint
+// string=>""
+// boolean=> true/false
+// null=>standalone value
+// undefined=>
+// symbol=>unique 
+
+/* premetive data type */
+// object=>
+
+// console.log(typeof"Pritam")//string
+// console.log(typeof null)//object
+// console.log(typeof undefined)//undefined
+
