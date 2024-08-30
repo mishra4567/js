76adf51fc59f025b0aabc4163a0397ab6f5b70ba
mishra4567 <mishrapritam831@gmail.com>
2024-08-30 21:15:06 +0530

    fw

--- 06_dom/one.js
+++
@@ -1,5 +0,0 @@
-// document.getElementById('title')
-// document.getElementById('title').id
-// document.getElementById('title').class
-// document.getElementById('title').className
-// document.getElementById('title').getAttribute
