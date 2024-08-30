5c233440c963925747c23f837abe502460d6a05e
mishra4567 <mishrapritam831@gmail.com>
2024-08-30 10:18:36 +0530

    How to create a new element in DOM

---
+++ 06_dom/one.js
@@ -0,0 +1,5 @@
+// document.getElementById('title')
+// document.getElementById('title').id
+// document.getElementById('title').class
+// document.getElementById('title').className
+// document.getElementById('title').getAttribute
